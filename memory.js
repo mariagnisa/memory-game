f6c259e8b17ce7c2b1c7139374bd5dbb579015ef
Maria Gustafsson Niså <maria.gnisa@gmail.com>
2016-11-04 21:54:13 +0100

    Added shuffle function in .js

--- memory.js
+++ memory.js
@@ -1,27 +1,35 @@
   'use strict';
 
-  let images = [];
-  images[0]="avatar/avatar_0.jpg";
-  images[1]="avatar/avatar_1.jpg";
-  images[2]="avatar/avatar_2.jpg";
-  images[3]="avatar/avatar_3.jpg";
-  images[4]="avatar/avatar_4.jpg";
-  images[5]="avatar/avatar_5.jpg";
-  images[6]="avatar/avatar_6.jpg";
-  images[7]="avatar/avatar_7.jpg";
+  let images = [
+  "avatar/avatar_0.jpg",
+  "avatar/avatar_1.jpg",
+  "avatar/avatar_2.jpg",
+  "avatar/avatar_3.jpg",
+  "avatar/avatar_4.jpg",
+  "avatar/avatar_5.jpg",
+  "avatar/avatar_6.jpg",
+  "avatar/avatar_7.jpg",
+  "avatar/avatar_0.jpg",
+  "avatar/avatar_1.jpg",
+  "avatar/avatar_2.jpg",
+  "avatar/avatar_3.jpg",
+  "avatar/avatar_4.jpg",
+  "avatar/avatar_5.jpg",
+  "avatar/avatar_6.jpg",
+  "avatar/avatar_7.jpg"
+];
 
+let card_values = [];
+let card_id = [];
+let cards_flip = 0;
 
-  for (let i = 0; i < images.length; i++) {
-    let rand = Math.floor((Math.random() * images.length) + 1);
-    let img = 'avatar/avatar_' + rand + '.jpg';
+// Shuffle the cards
+Array.prototype.memory_card_shuffle = function () {
+  let i = this.length, rand, temp;
+  while (--i > 0) {
+    rand = Math.floor(Math.random() * (i + 1));
+    temp = this[rand];
+    this[rand] = this[i];
+    this[i] = temp;
   }
-
-  let output = "<ol>";
-  for (let j = 0; j < 16; j++) {
-    output += "<li>";
-    output += "<img src = '" + images[i] + "'/>";
-    output += "</li>";
-  }
-  output += "</ol>";
-  document.getElementById("cardHolder").innerHTML = output;
-  $("img").hide();
+};
